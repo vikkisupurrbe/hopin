5aae42d2d4e7b0e123c9a51891d7497ff53b799b
vikkisupurrbe <yingjialiu1216@gmail.com>
2025-06-16 17:15:37 -0700

    Implement star rating

--- app/src/components/BeerCard.jsx
+++ app/src/components/BeerCard.jsx
@@ -1,9 +1,28 @@
+import { FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
+
 function BeerCard({ beer }) {
+
+  const renderStars = (rating) => {
+    let stars = [];
+
+    for (let i = 1; i <= 5; i++) {
+      if (rating >= i) {
+        stars.push(<FaStar key={i} />);
+      } else if (rating >= i - 0.5) {
+        stars.push(<FaStarHalfAlt key={i} />);
+      } else {
+        stars.push(<FaRegStar key={i} />);
+      }
+    }
+
+    return stars;
+  };
+
   return <div>
     <h4>{beer.name}</h4>
-    <p>{beer.brewery}</p>
-    <p>{beer.rating}</p>
-  </div>;
+    <p>Brewery: {beer.brewery}</p>
+    <span>{renderStars(beer.rating)}</span>
+  </div>
 }
 
 export default BeerCard;
