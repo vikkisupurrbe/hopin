dfb77c93dee595a0a2908a522536e27854475616
vikkisupurrbe <yingjialiu1216@gmail.com>
2025-07-07 15:57:51 -0700

    Implement create beer

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,14 +1,39 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from 'axios';
 import BeerList from "./components/BeerList";
-import beerData from '../../data/beers.json';
+import BeerCreate from "./components/BeerCreate";
 
 
 function App() {
-  const [beers, setBeers] = useState(beerData);
+  const [beers, setBeers] = useState([]);
+
+  const fetchBeers = async () => {
+    const response = await axios.get('http://localhost:3002/beers');
+    setBeers(response.data);
+  }
+
+  useEffect (() => {
+    fetchBeers();
+  }, []);
+
+  const createBeer = async (name, brewery, rating) => {
+    const response = await axios.post('http://localhost:3002/beers', {
+      name, brewery, rating
+    });
+
+    console.log(response);
+
+    const updatedBeers = [
+      ...beers,
+      response.data
+    ];
+    setBeers(updatedBeers);
+  }
 
   return <div>
     <h1>Welcome to Hopin!</h1>
     <p>"— Hop in. Sip. Rate. Repeat."</p>
+    <BeerCreate onCreate={createBeer} />
     <BeerList beers={beers} />
   </div>;
 }
---
+++ app/src/components/BeerCreate.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+
+function BeerCreate ({ onCreate }) {
+
+  const [name, setName] = useState('');
+  const [brewery, setBrewery] = useState('');
+  const [rating, setRating] = useState('');
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleBreweryChange = (event) => {
+    setBrewery(event.target.value);
+  };
+
+  const handleRatingChange = (event) => {
+    setRating(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onCreate(name, brewery, Number(rating)); // convert to number
+    setName('');
+    setBrewery('');
+    setRating('');
+  };
+
+  return (
+    <div>
+      <h3>Add a Beer</h3>
+      <form onSubmit={handleSubmit}>
+        <label>Name: </label>
+        <input value={name} onChange={handleNameChange} />
+        <label>Brewery: </label>
+        <input value={brewery} onChange={handleBreweryChange} />
+        <label>Rating: </label>
+        <input value={rating} onChange={handleRatingChange} />
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+};
+
+export default BeerCreate;
